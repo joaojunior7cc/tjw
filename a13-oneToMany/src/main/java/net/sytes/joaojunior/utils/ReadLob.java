052438a7190bd38f628f133ec5af058d4c83eb58
Joao Junior <joao.junior7cc@gmail.com>
2020-09-13 10:54:36 -0300

    one-to-many

---
+++ a13-oneToMany/src/main/java/net/sytes/joaojunior/utils/ReadLob.java
@@ -0,0 +1,25 @@
+package net.sytes.joaojunior.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ReadLob {
+	
+	public byte[] getFoto(String path) throws IOException {
+		File file = new File(path);
+		byte[] foto = new byte[(int) file.length()];
+		FileInputStream fileInputStream = null;
+		try {
+			fileInputStream = new FileInputStream(file);
+			fileInputStream.read(foto);
+			fileInputStream.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return foto;
+	}
+
+}

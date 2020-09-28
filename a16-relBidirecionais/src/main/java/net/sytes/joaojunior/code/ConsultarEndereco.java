313b41943d7fd7fd15149d8bdc7c2f1450f20eba
Joao Junior <joao.junior7cc@gmail.com>
2020-09-28 18:41:41 -0300

    Relacionamento Bidirecional

---
+++ a16-relBidirecionais/src/main/java/net/sytes/joaojunior/code/ConsultarEndereco.java
@@ -0,0 +1,17 @@
+package net.sytes.joaojunior.code;
+
+import javax.persistence.EntityManager;
+
+import net.sytes.joaojunior.model.Endereco;
+import net.sytes.joaojunior.utils.JPAUtil;
+
+public class ConsultarEndereco {
+	public static void main(String args[]) {
+		EntityManager manager = JPAUtil.getEntityManager();
+		Endereco endereco = manager.find(Endereco.class, 6L);
+		System.out.println("######################################");
+		System.out.println(endereco.getCidade());
+		System.out.println(endereco.getBairro());
+	}
+
+}

7b533b3256c5fa8aa629151d2b5dcc10046fc9e1
Joao Junior <joao.junior7cc@gmail.com>
2020-10-02 14:00:10 -0300

    EllementCollection

---
+++ a17-JPQL1e2/src/main/java/net/sytes/joaojunior/code/ConsultarEndereco.java
@@ -0,0 +1,14 @@
+package net.sytes.joaojunior.code;
+
+import javax.persistence.EntityManager;
+
+import net.sytes.joaojunior.model.Endereco;
+import net.sytes.joaojunior.utils.JPAUtil;
+
+public class ConsultarEndereco {
+	public static void main(String args[]) {
+		EntityManager manager = JPAUtil.getEntityManager();
+
+	}
+
+}
